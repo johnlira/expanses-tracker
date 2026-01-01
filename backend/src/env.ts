9669b64490ca245b50771889b3d89fad296451bb
John Lira <johnalsoares@gmail.com>
2026-01-01 20:00:41 -0300

    feat (backend):  prisma's setup

--- backend/src/env.ts
+++ backend/src/env.ts
@@ -5,6 +5,7 @@ const envSchema = z.object({
   PORT: z.coerce.number("PORT is required").default(3000),
   HOST: z.string("HOST is required").default("localhost"),
   JWT_SECRET: z.string("JWT_SECRET is required").min(1),
+  DATABASE_URL: z.string("DATABASE_URL is required").min(1),
 });
 
 export const env = envSchema.parse(process.env);
